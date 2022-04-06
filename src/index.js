68117437e66847e2f3fa47c2a1b76e3911f61b0d
reema09 <77222281+reema09@users.noreply.github.com>
2022-04-06 12:17:03 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AtmGroupType", {
+  enumerable: true,
+  get: function get() {
+    return AtmGroupType.default;
+  }
+});
+var _AtmGroupType = _interopRequireDefault(require("./AtmGroupType"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
