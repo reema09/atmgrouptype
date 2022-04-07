62c638de518a142c833ee3b709b2a7510181076a
reema09 <77222281+reema09@users.noreply.github.com>
2022-04-07 12:34:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", {
 Object.defineProperty(exports, "AtmGroupType", {
   enumerable: true,
   get: function get() {
-    return AtmGroupType.default;
+    return _AtmGroupType.default;
   }
 });
+
 var _AtmGroupType = _interopRequireDefault(require("./AtmGroupType"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
